421dff54e26926a980448653fba80eb86c3b5b64
Shreyash Galgale <shreyasgalgale019@gmail.com>
2024-04-09 00:01:11 +0530

    WeatherApp

---
+++ script.js
@@ -0,0 +1,29 @@
+const API_KEY = "7de2778808ed56eeb94af5394ed84e8f";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+
+async function getWeather(city) {
+    try {
+        let response = await fetch(
+            API_URL + `&q=${city}` + `&appid=${API_KEY}`
+        );
+        let data = await response.json();
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = `${data.main.temp}&#8451`;
+        document.querySelector(".humidity").innerHTML =
+            data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
+
+        if (data.weather[0].main.toLowerCase() === "Clear".toLowerCase()) {
+            document.querySelector(".weather-icon").src = "imgs/sun.png";
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+document.querySelector("#btn-search").addEventListener("click", function () {
+    let city = document.querySelector(".inputCity");
+    getWeather(city.value);
+});
